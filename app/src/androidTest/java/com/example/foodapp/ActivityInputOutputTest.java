90abaef02fd0cd5157620362d1983f519f7ff52e
ripesh28 <ripeshyadav95@gmail.com>
2020-06-15 19:38:38 +0530

    Initial commit

---
+++ app/src/androidTest/java/com/example/foodapp/ActivityInputOutputTest.java
@@ -0,0 +1,43 @@
+package com.example.foodapp;
+
+import android.content.Context;
+
+import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.rule.ActivityTestRule;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static org.junit.Assert.*;
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+public class ActivityInputOutputTest{
+
+    @Rule
+    public ActivityTestRule mActivityRule = new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void activityLaunch()
+    {
+        onView(withId(R.id.order)).perform(click());
+        onView(withId(R.id.order_text)).check(matches(isDisplayed()));
+    }
+    @Test
+    public void useAppContext() {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
+
+        assertEquals("com.example.foodapp", appContext.getPackageName());
+    }
+}
